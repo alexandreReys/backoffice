b91705dc2266610087e538c5335799bf85d6005e
alexandrereys <anr.alexandre@gmail.com>
2024-10-26 17:01:08 -0300

    feat: add company model

---
+++ prisma/migrations/20241026143956_add_companies_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
