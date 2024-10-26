3e7d0b790a7b9db44ed3e194e37637167411e021
alexandrereys <anr.alexandre@gmail.com>
2024-10-26 17:02:20 -0300

    feat: add new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { UsersModule } from '@/modules/users/users.module';
+import { AuthModule } from '@/modules/auth/auth.module';
+import { CompanyModule } from '@/modules/companies/company.module';
+import { TokenMiddleware } from '@/middlewares/token.middleware';
+import { ContextModule } from '@/modules/request-context/context.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    CompanyModule,
+    ContextModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TokenMiddleware).forRoutes('*');
+  }
+}
